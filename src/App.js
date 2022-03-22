c3460cdff91cd1c2fbe0d3843bef2e5a2371ee38
Billy Endres <endres63@hotmail.com>
2022-03-23 09:13:27 +1100

    Project layout and React Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="bg-green-500 text-center text-5xl p-6">background</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </>
   );
 };
 
