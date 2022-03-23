e41546938aac768a55b5c69dbea43f41c1469802
Billy Endres <endres63@hotmail.com>
2022-03-23 22:44:28 +1100

    Responsive navbar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,82 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
+const menuItems = [
+  {
+    title: "Home",
+    key: "/",
+  },
+  {
+    title: "Projects",
+    key: "/projects",
+  },
+  {
+    title: "Courses",
+    key: "/courses",
+  },
+  {
+    title: "Contact",
+    key: "/contact",
+  },
+];
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState("md:hidden");
+  const pathname = window.location.pathname;
+
   return (
-    <div>
-      <h1 className="bg-github text-white font-mont text-center p-4">
-        This is the header
-      </h1>
+    <div className="text-white font-mont">
+      <div
+        className={`flex bg-github justify-between items-center p-2 font-semibold shadow-lg ${
+          showMenu === "" && "md:flex-col"
+        }`}
+      >
+        <div className="flex justify-between w-full items-center">
+          <h1 className="text-4xl">BILLY ENDRES</h1>
+
+          <FaBars
+            onClick={() => {
+              showMenu === "md:hidden"
+                ? setShowMenu("")
+                : setShowMenu("md:hidden");
+            }}
+            className="lg:hidden xl:hidden 2xl:hidden md:flex cursor-pointer"
+          />
+        </div>
+        <div className="flex md:hidden">
+          {menuItems.map(({ title, key }) => {
+            return (
+              <li
+                key={key}
+                className={`list-none mx-5 ${
+                  key === pathname && "bg-white text-black rounded-md px-3"
+                }`}
+              >
+                <Link to={`${key}`}>{title}</Link>
+              </li>
+            );
+          })}
+        </div>
+
+        <div
+          className={`mt-5 md:flex items-start w-full flex-col lg:hidden xl:hidden 2xl:hidden ${showMenu}`}
+        >
+          {menuItems.map(({ title, key }) => {
+            return (
+              <li
+                key={key}
+                className={`list-none p-2 ${
+                  key === pathname && "bg-white text-black rounded-md"
+                }`}
+              >
+                <Link to={`${key}`}>{title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,11 @@ module.exports = {
       },
     },
     screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
+      "2xl": { max: "5350px" },
       xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
       lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
       md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-
       sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
     },
   },
   plugins: [],
